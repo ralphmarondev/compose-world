36fed4528a03305ae2b07ad96033e3c4bd526c45
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-11-15 22:36:11 +0800

    [keepr] added navigation drawer

--- features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/home/HomeScreen.kt
+++ features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/home/HomeScreen.kt
@@ -9,17 +9,21 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AddCircleOutline
 import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
@@ -28,8 +32,10 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ralphmarondev.keepr.data.local.KeeprDao
 import com.ralphmarondev.keepr.presentation.components.CategoryCard
+import com.ralphmarondev.keepr.presentation.home.components.DrawerContent
 import com.ralphmarondev.keepr.presentation.home.components.NewCategoryDialog
 import com.ralphmarondev.keepr.util.getCardImage
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -48,79 +54,94 @@ fun HomeScreen(
     val showNewDialog by viewModel.showNewDialog.collectAsState()
 
     val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Hello, $currentUser",
-                        fontFamily = FontFamily.Monospace,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1
-                    )
-                },
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(
-                            imageVector = Icons.Outlined.Menu,
-                            contentDescription = "Menu"
+    ModalNavigationDrawer(
+        drawerContent = {
+            DrawerContent(closeDrawer = { scope.launch { drawerState.apply { close() } } })
+        },
+        drawerState = drawerState
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "Hello, $currentUser",
+                            fontFamily = FontFamily.Monospace,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1
                         )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { viewModel.toggleShowNewDialog() }) {
-                        Icon(
-                            imageVector = Icons.Outlined.AddCircleOutline,
-                            contentDescription = "New Category"
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
-                )
-            )
-        }
-    ) { innerPadding ->
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(8.dp)
-        ) {
-            items(categories) { category ->
-                CategoryCard(
-                    image = getCardImage(category.name),
-                    text = category.name,
-                    onClick = {
-                        navigateToSubCategory(category.name)
                     },
-                    modifier = Modifier
-                        .padding(8.dp)
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                scope.launch {
+                                    drawerState.apply { open() }
+                                }
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Menu,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { viewModel.toggleShowNewDialog() }) {
+                            Icon(
+                                imageVector = Icons.Outlined.AddCircleOutline,
+                                contentDescription = "New Category"
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                        actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                        navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
+                    )
                 )
             }
-        }
-
-        if (showNewDialog) {
-            NewCategoryDialog(
-                onDismiss = { viewModel.toggleShowNewDialog() },
-                onSaveCategory = { name ->
-                    viewModel.createNewCategory(
-                        name = name,
-                        response = { isSuccess, msg ->
-                            if (isSuccess) {
-                                viewModel.toggleShowNewDialog()
-                            } else {
-                                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
-                            }
-                        }
+        ) { innerPadding ->
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(8.dp)
+            ) {
+                items(categories) { category ->
+                    CategoryCard(
+                        image = getCardImage(category.name),
+                        text = category.name,
+                        onClick = {
+                            navigateToSubCategory(category.name)
+                        },
+                        modifier = Modifier
+                            .padding(8.dp)
                     )
                 }
-            )
+            }
+
+            if (showNewDialog) {
+                NewCategoryDialog(
+                    onDismiss = { viewModel.toggleShowNewDialog() },
+                    onSaveCategory = { name ->
+                        viewModel.createNewCategory(
+                            name = name,
+                            response = { isSuccess, msg ->
+                                if (isSuccess) {
+                                    viewModel.toggleShowNewDialog()
+                                } else {
+                                    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        )
+                    }
+                )
+            }
         }
     }
 }
---
+++ features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/home/components/DrawerContent.kt
@@ -0,0 +1,213 @@
+package com.ralphmarondev.keepr.presentation.home.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Backup
+import androidx.compose.material.icons.outlined.Category
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.ImportExport
+import androidx.compose.material.icons.outlined.ManageAccounts
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.StarOutline
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.ralphmarondev.keepr.R
+
+@Composable
+fun DrawerContent(
+    closeDrawer: () -> Unit
+) {
+    val drawerItems1 = listOf(
+        NavigationModel(
+            icon = Icons.Outlined.Person,
+            label = "My Profile",
+            onClick = {}
+        ),
+        NavigationModel(
+            icon = Icons.Outlined.Category,
+            label = "Categories",
+            onClick = {}
+        ),
+        NavigationModel(
+            icon = Icons.Outlined.StarOutline,
+            label = "SubCategories",
+            onClick = {}
+        ),
+        NavigationModel(
+            icon = Icons.Outlined.ManageAccounts,
+            label = "Manage Accounts",
+            onClick = {}
+        )
+    )
+
+    val drawerItems2 = listOf(
+        NavigationModel(
+            icon = Icons.Outlined.Backup,
+            label = "Backup",
+            onClick = {}
+        ),
+        NavigationModel(
+            icon = Icons.Outlined.ImportExport,
+            label = "Export",
+            onClick = {}
+        )
+    )
+
+    ModalDrawerSheet(
+        modifier = Modifier
+            .statusBarsPadding()
+            .systemBarsPadding()
+            .padding(end = 48.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .background(MaterialTheme.colorScheme.primaryContainer)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(R.drawable.keepr),
+                    contentDescription = "App Icon",
+                    modifier = Modifier
+                        .size(100.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "Keepr",
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.W500,
+                    modifier = Modifier
+                        .padding(vertical = 4.dp),
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Text(
+                    text = "Version 1.0",
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight.W400,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+
+            Spacer(modifier = Modifier.height(4.dp))
+            drawerItems1.forEachIndexed { _, item ->
+                NavigationDrawerItem(
+                    label = {
+                        Text(
+                            text = item.label,
+                            fontFamily = FontFamily.Monospace
+                        )
+                    },
+                    selected = false,
+                    onClick = item.onClick,
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.label
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp),
+                    colors = NavigationDrawerItemDefaults.colors(
+                        unselectedTextColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                        unselectedIconColor = MaterialTheme.colorScheme.onTertiaryContainer
+                    )
+                )
+            }
+
+            HorizontalDivider(modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp))
+            drawerItems2.forEachIndexed { _, item ->
+                NavigationDrawerItem(
+                    label = {
+                        Text(
+                            text = item.label,
+                            fontFamily = FontFamily.Monospace
+                        )
+                    },
+                    selected = false,
+                    onClick = item.onClick,
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.label
+                        )
+                    },
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp),
+                    colors = NavigationDrawerItemDefaults.colors(
+                        unselectedTextColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                        unselectedIconColor = MaterialTheme.colorScheme.onTertiaryContainer
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+            HorizontalDivider(modifier = Modifier.padding(horizontal = 4.dp, vertical = 8.dp))
+            NavigationDrawerItem(
+                label = {
+                    Text(
+                        text = "Close Drawer",
+                        fontFamily = FontFamily.Monospace
+                    )
+                },
+                onClick = closeDrawer,
+                selected = false,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Outlined.Close,
+                        contentDescription = "Close"
+                    )
+                },
+                modifier = Modifier
+                    .padding(8.dp),
+                colors = NavigationDrawerItemDefaults.colors(
+                    unselectedContainerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                    unselectedTextColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                    unselectedIconColor = MaterialTheme.colorScheme.onTertiaryContainer
+                )
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+private data class NavigationModel(
+    val icon: ImageVector,
+    val label: String,
+    val onClick: () -> Unit
+)
