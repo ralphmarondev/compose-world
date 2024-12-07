c0834a5759fcd44948aeb5a7ee2a5f4a68ba878e
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-12-07 22:51:17 +0800

    [keepr] added CardDetails on UpdateScreen

--- features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/update/UpdateScreen.kt
+++ features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/update/UpdateScreen.kt
@@ -1,10 +1,19 @@
 package com.ralphmarondev.keepr.presentation.update
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,8 +23,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.ralphmarondev.keepr.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -51,9 +68,59 @@ fun UpdateScreen(
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
+            item { Spacer(modifier = Modifier.height(4.dp)) }
+            items(2) {
+                CardDetails(
+                    image = R.drawable.facebook,
+                    text = "Facebook",
+                    onClick = {}
+                )
+            }
+            item { Spacer(modifier = Modifier.height(100.dp)) }
+        }
+    }
+}
 
+@Composable
+private fun CardDetails(
+    modifier: Modifier = Modifier,
+    image: Int,
+    text: String,
+    onClick: () -> Unit
+) {
+    ElevatedCard(
+        onClick = onClick,
+        modifier = modifier
+            .widthIn(max = 500.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(image),
+                contentDescription = text,
+                modifier = Modifier
+                    .size(76.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Text(
+                text = text,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.W500,
+                fontSize = 18.sp,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
         }
     }
 }
