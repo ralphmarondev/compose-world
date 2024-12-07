93ba95c463711a4ab61f7a0b16e72f6773d2007e
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-12-07 21:33:45 +0800

    [keepr] make use of BottomSheet instead of dialog in displaying account details

--- features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/details/DetailScreen.kt
+++ features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/details/DetailScreen.kt
@@ -1,20 +1,34 @@
 package com.ralphmarondev.keepr.presentation.details
 
 import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material.icons.outlined.ContentCopy
+import androidx.compose.material.icons.outlined.DeleteForever
+import androidx.compose.material.icons.outlined.DoneAll
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -25,15 +39,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ralphmarondev.keepr.data.local.KeeprDao
 import com.ralphmarondev.keepr.domain.model.Account
 import com.ralphmarondev.keepr.presentation.details.components.AccountCard
-import com.ralphmarondev.keepr.presentation.details.components.AccountDetailsDialog
 import com.ralphmarondev.keepr.presentation.details.components.NewAccountDialog
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -115,6 +137,7 @@ fun DetailScreen(
                     ),
                     onClick = {
                         selectedAccount = Account(
+                            id = account.id,
                             name = account.name,
                             username = account.username,
                             password = account.password,
@@ -151,10 +174,207 @@ fun DetailScreen(
         }
 
         if (showAccountDetailsDialog) {
-            AccountDetailsDialog(
+            AccountDetailsBottomSheet(
                 account = selectedAccount,
                 onDismiss = { showAccountDetailsDialog = !showAccountDetailsDialog }
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AccountDetailsBottomSheet(
+    onDismiss: () -> Unit,
+    account: Account
+) {
+    val context = LocalContext.current
+    val clipboardManager = LocalClipboardManager.current
+    var accountLabel by remember { mutableStateOf(account.name) }
+    var username by remember { mutableStateOf(account.username) }
+    var password by remember { mutableStateOf(account.password) }
+    var showPassword by remember { mutableStateOf(false) }
+
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Details",
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.W500,
+                fontSize = 20.sp,
+                color = MaterialTheme.colorScheme.primary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            HorizontalDivider(modifier = Modifier.padding(vertical = 4.dp))
+
+            Text(
+                text = "Account Label",
+                fontFamily = FontFamily.Monospace,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.W300,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+            OutlinedTextField(
+                value = accountLabel,
+                onValueChange = { accountLabel = it },
+                placeholder = {
+                    Text(
+                        text = "Account label",
+                        fontFamily = FontFamily.Monospace,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                },
+                textStyle = TextStyle(
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W500,
+                    color = MaterialTheme.colorScheme.secondary
+                ),
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 4.dp))
+
+            Text(
+                text = "Username",
+                fontFamily = FontFamily.Monospace,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.W300,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                placeholder = {
+                    Text(
+                        text = "Username",
+                        fontFamily = FontFamily.Monospace,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                },
+                textStyle = TextStyle(
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W500,
+                    color = MaterialTheme.colorScheme.secondary
+                ),
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 4.dp))
+
+            Text(
+                text = "Password",
+                fontFamily = FontFamily.Monospace,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.W300,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                placeholder = {
+                    Text(
+                        text = "Username",
+                        fontFamily = FontFamily.Monospace,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                },
+                textStyle = TextStyle(
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W500,
+                    color = MaterialTheme.colorScheme.secondary
+                ),
+                singleLine = true,
+                trailingIcon = {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        AnimatedVisibility(account.password.isNotEmpty()) {
+                            IconButton(
+                                onClick = { showPassword = !showPassword }
+                            ) {
+                                val icon =
+                                    if (showPassword) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff
+                                Icon(
+                                    imageVector = icon,
+                                    contentDescription = "Password Visibility"
+                                )
+                            }
+                        }
+                    }
+                },
+                visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            HorizontalDivider(modifier = Modifier.padding(top = 8.dp, bottom = 4.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                IconButton(
+                    onClick = {
+                        clipboardManager.setText(
+                            AnnotatedString(
+                                text = "Username: ${account.username}\nPassword: ${account.password}"
+                            )
+                        )
+                        Toast.makeText(context, "Copied!", Toast.LENGTH_SHORT).show()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.ContentCopy,
+                        contentDescription = "Copy",
+                        tint = MaterialTheme.colorScheme.secondary
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        Toast.makeText(context, "Deleted!", Toast.LENGTH_SHORT).show()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.DeleteForever,
+                        contentDescription = "Delete",
+                        tint = MaterialTheme.colorScheme.secondary
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(
+                    onClick = {
+                        Toast.makeText(context, "Updated", Toast.LENGTH_SHORT).show()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.DoneAll,
+                        contentDescription = "Save",
+                        tint = MaterialTheme.colorScheme.secondary
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(100.dp))
         }
     }
 }
