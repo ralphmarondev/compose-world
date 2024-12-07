0b1ea30b9f2e414aad178f45bec82d85c69400ac
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-12-07 23:24:35 +0800

    [keepr] added BottomSheet on UpdateScreen and added UpdateViewModel

--- features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/update/UpdateScreen.kt
+++ features/keepr/src/main/java/com/ralphmarondev/keepr/presentation/update/UpdateScreen.kt
@@ -2,6 +2,7 @@ package com.ralphmarondev.keepr.presentation.update
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,28 +10,41 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material.icons.outlined.DeleteForever
+import androidx.compose.material.icons.outlined.DoneAll
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import com.ralphmarondev.keepr.R
 
@@ -40,6 +54,10 @@ fun UpdateScreen(
     title: String,
     navigateBack: () -> Unit
 ) {
+    val viewModel: UpdateViewModel = viewModel()
+    val selectedItem by viewModel.selectedItem.collectAsState()
+    val showBottomSheet by viewModel.showBottomSheet.collectAsState()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -76,11 +94,27 @@ fun UpdateScreen(
                 CardDetails(
                     image = R.drawable.facebook,
                     text = "Facebook",
-                    onClick = {}
+                    onClick = {
+                        viewModel.onSelectedItemChange("Facebook")
+                        viewModel.toggleShowBottomSheet()
+                    }
                 )
             }
             item { Spacer(modifier = Modifier.height(100.dp)) }
         }
+    }
+
+    if (showBottomSheet) {
+        DetailsBottomSheet(
+            onDismiss = {
+                viewModel.toggleShowBottomSheet()
+            },
+            label = title,
+            text = selectedItem,
+            onUpdate = { value ->
+
+            }
+        )
     }
 }
 
@@ -121,6 +155,103 @@ private fun CardDetails(
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DetailsBottomSheet(
+    onDismiss: () -> Unit,
+    label: String,
+    text: String,
+    onUpdate: (String) -> Unit
+) {
+    var value by remember { mutableStateOf(text) }
+    val labelStr = if (label == "Categories") "Category" else "Subcategory"
+
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = labelStr,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.W500,
+                fontSize = 20.sp,
+                color = MaterialTheme.colorScheme.primary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            HorizontalDivider(modifier = Modifier.padding(vertical = 4.dp))
+
+            Text(
+                text = "$labelStr Name",
+                fontFamily = FontFamily.Monospace,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.W300,
+                color = MaterialTheme.colorScheme.tertiary
+            )
+            OutlinedTextField(
+                value = value,
+                onValueChange = { value = it },
+                placeholder = {
+                    Text(
+                        text = "$labelStr name",
+                        fontFamily = FontFamily.Monospace,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                },
+                textStyle = TextStyle(
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.W500,
+                    color = MaterialTheme.colorScheme.secondary
+                ),
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            HorizontalDivider(modifier = Modifier.padding(top = 8.dp, bottom = 4.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(
+                    onClick = { }
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.DeleteForever,
+                        contentDescription = "Delete",
+                        tint = MaterialTheme.colorScheme.secondary
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        onUpdate(value)
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.DoneAll,
+                        contentDescription = "Save",
+                        tint = MaterialTheme.colorScheme.secondary
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(100.dp))
         }
     }
 }
