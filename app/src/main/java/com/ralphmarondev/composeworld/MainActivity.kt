b697015ab71faac3fc7702ee380944dcb4e119b7
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-02 14:24:18 +0800

    updated app icon and added splashscreen support for lower sdk

--- app/src/main/java/com/ralphmarondev/composeworld/MainActivity.kt
+++ app/src/main/java/com/ralphmarondev/composeworld/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ralphmarondev.composeworld.ui.theme.ComposeWorldTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             var darkTheme by remember { mutableStateOf(false) }
 
