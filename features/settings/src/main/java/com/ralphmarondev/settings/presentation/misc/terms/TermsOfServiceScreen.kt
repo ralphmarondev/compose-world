3ffb4ad4bbfb462973c4da602550a7cccd322e2a
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-12 22:48:26 +0800

    updated `TermsOfServiceScreen`

--- features/settings/src/main/java/com/ralphmarondev/settings/presentation/misc/terms/TermsOfServiceScreen.kt
+++ features/settings/src/main/java/com/ralphmarondev/settings/presentation/misc/terms/TermsOfServiceScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -73,21 +74,27 @@ fun TermsOfServiceScreen(
                         color = MaterialTheme.colorScheme.primary
                     )
                     Text(
-                        text = "Last updated January 13, 2025",
+                        text = "Last updated March 12, 2025",
                         fontWeight = FontWeight.W300,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.tertiary
                     )
+                    HorizontalDivider(
+                        modifier = Modifier.padding(
+                            vertical = 8.dp
+                        )
+                    )
                     Text(
-                        text = "Welcome to Compose World! These Terms of Service govern " +
-                                "your use of the Compose World app, operated by Ralph Maron Eda. " +
-                                "By accessing or using the App, you agree to these Terms.",
+                        text = "Welcome to Compose World! These Terms of Service govern your use of " +
+                                "the Compose World app, developed and maintained by Ralph Maron " +
+                                "Eda. By accessing or using the App, you agree to these Terms.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -100,20 +107,23 @@ fun TermsOfServiceScreen(
                         fontSize = 16.sp,
                         color = MaterialTheme.colorScheme.primary
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "- You must use the App in compliance with all applicable laws and regulations.",
+                        text = "You must use the App in compliance with all applicable laws and regulations.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
+                    Spacer(modifier = Modifier.height(1.5.dp))
                     Text(
-                        text = "- The App is intended for personal use only and should not be used for unlawful purposes.",
+                        text = "The App is intended for personal use only and should not be used for unlawful purposes.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -126,8 +136,9 @@ fun TermsOfServiceScreen(
                         fontSize = 16.sp,
                         color = MaterialTheme.colorScheme.primary
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "- Your use of the App is also governed by our [Privacy Policy], please " +
+                        text = "Your use of the App is also governed by our [Privacy Policy], please " +
                                 "review it to understand how we collect, use, and protect your information.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
@@ -135,6 +146,7 @@ fun TermsOfServiceScreen(
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -147,21 +159,24 @@ fun TermsOfServiceScreen(
                         fontSize = 16.sp,
                         color = MaterialTheme.colorScheme.primary
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "- You are responsible for maintaining the security of your device and ensuring " +
+                        text = "You are responsible for maintaining the security of your device and ensuring " +
                                 "the confidentiality of your account.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
+                    Spacer(modifier = Modifier.height(1.5.dp))
                     Text(
-                        text = "- You agree not to reverse-engineer, decompile, or tamper with the App in any way.",
+                        text = "You agree not to reverse-engineer, decompile, or tamper with the App in any way.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -174,22 +189,25 @@ fun TermsOfServiceScreen(
                         fontSize = 16.sp,
                         color = MaterialTheme.colorScheme.primary
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "- The App may allow you to store, share or upload content. You retain " +
+                        text = "The App may allow you to store, share or upload content. You retain " +
                                 "ownership of any content you submit, but by doing so, you grant us a license " +
                                 "to use it for app-related purposes.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
+                    Spacer(modifier = Modifier.height(1.5.dp))
                     Text(
-                        text = "- Any inappropriate or illegal content is strictly prohibited.",
+                        text = "Any inappropriate or illegal content is strictly prohibited.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -203,20 +221,21 @@ fun TermsOfServiceScreen(
                         color = MaterialTheme.colorScheme.primary
                     )
                     Text(
-                        text = "- The App is provided 'as is' without warranties of any kind. We do not " +
+                        text = "The App is provided 'as is' without warranties of any kind. We do not " +
                                 "guarantee that the App will be error-free, secure, or continuously available.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
                     Text(
-                        text = "- We are not responsible for any data loss or damages arising from your use of the App.",
+                        text = "We are not responsible for any data loss or damages arising from your use of the App.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -229,8 +248,9 @@ fun TermsOfServiceScreen(
                         fontSize = 16.sp,
                         color = MaterialTheme.colorScheme.primary
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "- To the fullest extent permitted by law, we shall not be liable for any " +
+                        text = "To the fullest extent permitted by law, we shall not be liable for any " +
                                 "indirect, incidental, or consequential damages arising from your use of the App.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
@@ -238,6 +258,7 @@ fun TermsOfServiceScreen(
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -250,8 +271,9 @@ fun TermsOfServiceScreen(
                         fontSize = 16.sp,
                         color = MaterialTheme.colorScheme.primary
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "- We may suspend or terminate your access to the App at any time for any reason, " +
+                        text = "We may suspend or terminate your access to the App at any time for any reason, " +
                                 "including violations of these Terms.",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
@@ -259,6 +281,7 @@ fun TermsOfServiceScreen(
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -271,8 +294,9 @@ fun TermsOfServiceScreen(
                         fontSize = 16.sp,
                         color = MaterialTheme.colorScheme.primary
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "- We reserve the right to update these Terms at any time. Changes will be effective " +
+                        text = "We reserve the right to update these Terms at any time. Changes will be effective " +
                                 "immediately upon posting in the App. Your continued use of the App signifies " +
                                 "your acceptance of the updated Terms.",
                         fontWeight = FontWeight.W400,
@@ -281,6 +305,7 @@ fun TermsOfServiceScreen(
                     )
                 }
             }
+            item { HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp)) }
             item {
                 Column(
                     modifier = Modifier
@@ -293,8 +318,9 @@ fun TermsOfServiceScreen(
                         fontSize = 16.sp,
                         color = MaterialTheme.colorScheme.primary
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "- If you have any questions about these Terms, please contact us at edaralphmaron@gmail.com",
+                        text = "If you have any questions about these Terms, please contact us at edaralphmaron@gmail.com",
                         fontWeight = FontWeight.W400,
                         fontSize = 14.sp,
                         color = MaterialTheme.colorScheme.secondary
