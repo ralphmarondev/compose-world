83b6f9076a22f1e8ee12205ec03a8fe5d6a1729d
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-14 08:05:59 +0800

    fixed error on note app
    - We just forgot to annotate the database object.

--- features/notes/src/main/java/com/ralphmarondev/notes/data/local/NotesDatabase.kt
+++ features/notes/src/main/java/com/ralphmarondev/notes/data/local/NotesDatabase.kt
@@ -1,10 +1,18 @@
 package com.ralphmarondev.notes.data.local
 
 import android.content.Context
+import android.util.Log
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.ralphmarondev.notes.data.local.dao.NoteDao
+import com.ralphmarondev.notes.domain.model.Note
 
+@Database(
+    entities = [Note::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class NoteDatabase : RoomDatabase() {
     abstract val noteDao: NoteDao
 
@@ -13,13 +21,16 @@ abstract class NoteDatabase : RoomDatabase() {
 
         fun createDatabase(context: Context): NoteDatabase {
             try {
+                Log.d("App", "Creating database...")
                 val database = Room.databaseBuilder(
                     context,
                     NoteDatabase::class.java,
                     NAME
                 ).build()
+                Log.d("App", "Database created successfully.")
                 return database
             } catch (e: Exception) {
+                Log.e("App", "Error creating database: ${e.message}")
                 throw e
             }
         }
